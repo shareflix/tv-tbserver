066dfa6b89a0ff0ed3dc015db3903bac233eb5a4
shareflix <ttiby79@gmail.com>
2026-05-21 00:14:36 +0100

    Add files via upload

---
+++ service.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "TV.TBSERVER",
+  description: "TV.TBSERVER Online",
+  version: "1.0.0",
+
+  run: async () => {
+    window.location.href =
+      "https://tv.tbserver.online/tizenbrew/index.html";
+  }
+};
