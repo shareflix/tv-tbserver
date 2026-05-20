68cf9cdbf36dc839f86aa85733d829432dbc2cb9
shareflix <ttiby79@gmail.com>
2026-05-21 00:24:37 +0100

    Update service.js

--- service.js
+++ service.js
@@ -1,10 +1,1 @@
-module.exports = {
-  title: "TV.TBSERVER",
-  description: "TV.TBSERVER Online",
-  version: "1.0.0",
-
-  run: async () => {
-    window.location.href =
-      "https://tv.tbserver.online/tizenbrew/index.html";
-  }
-};
+module.exports = {};
